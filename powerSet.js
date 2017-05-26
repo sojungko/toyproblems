cc4b6bc172e904c4c75de8a592fd4b501221e9eb
Sojung Park <sojungko@gmail.com>
2017-05-26 19:59:03 -0400

    Solve powerSet, spiralTraversal

--- powerSet.js
+++ powerSet.js
@@ -1,31 +1,9 @@
-// const powerSet = (str) => {
-//   let result = { "": true };
-//   for (let i = 0; i < str.length; i++) {
-//     result[str[i]] = true;
-//   }
-//   for (prop in result) {
-//     for (let j = 0; j < str.length; j++) {
-//       let newProp = prop + str[j];
-//       if (!result.hasOwnProperty(newProp)) {
-//         result[newProp] = true;
-//       }
-//     }
-//   }
-//   return Object.keys(result).sort();
-// }
-
 const powerSet = (str) => {
-  let strArr = [...str];
-  let result = { "": true };
+  // Generate array of unique letters
+  let letters = Array.from(new Set(str)).sort();
   
-  const recurse = (string, index) => {
-    result[string] = true;
-    recurse(string + str[index], index);
-  }
-
-  for (let i = 0; i < strArr.length; i++) {
-    recurse("", i);
-  }
-
+  return letters.reduce((acc, curr) => {
+    return [...acc, ...acc.map(letter => letter + curr)]
+  }, ['']).sort();
 
 }
--- spiralTraversal.js
+++ spiralTraversal.js
@@ -1,23 +1,20 @@
-spiralTraversal = (matrix) => {
+const spiralTraversal = (matrix) => {
   let results = [];
   const recurse = (array) => {
-    if (array.length === 0) {
+    if (array.length === 0 || array[0].length === 0) {
       return;
     }
-    console.log('[...results, ...array.shift()] : ', [...results, ...array.shift()]);
     results = [...results, ...array.shift()];
-    console.log('results #1 : ', results);
     array.forEach((element) => {
       results.push(element.pop())
     });
-    console.log('results #2 : ', results);
-    console.log('[...results, ...array.pop().reverse()] : ', [...results, ...array.pop().reverse()]);
-    results = [...results, ...array.pop().reverse()];
-    console.log('results #3 : ', results);
+    results = array.length ? [...results, ...array.pop().reverse()] : results;
     for (let i = array.length - 1; i >= 0; i--) {
-      results.push(array[i].shift());
+      if (array[i].length) {
+        results.push(array[i].shift());
+      }
     }
-    console.log('results #4 : ', results);
+    recurse(array);
   }
   recurse(matrix);
   return results;
